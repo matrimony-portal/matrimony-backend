9d93b68c86e16e363959c80c6fdfbc03d572ee1b
Sachin Kumar <sinha1abc@gmail.com>
2026-01-24 02:14:04 +0530

    spring boot intial setup

---
+++ src/test/java/com/scriptbliss/bandhan/MatrimonyBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scriptbliss.bandhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrimonyBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
