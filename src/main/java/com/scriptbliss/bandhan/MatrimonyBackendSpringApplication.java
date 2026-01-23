408db16c5ceeefdee0eaefdcb5bc5dd5e181e878
Sachin Kumar <sinha1abc@gmail.com>
2026-01-24 02:14:04 +0530

    spring boot intial setup

---
+++ src/main/java/com/scriptbliss/bandhan/MatrimonyBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.scriptbliss.bandhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrimonyBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrimonyBackendSpringApplication.class, args);
+	}
+
+}
