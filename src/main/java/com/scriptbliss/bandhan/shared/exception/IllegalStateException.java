6d57c12a448e77a58b4b83e81cf1345518ec9744
Sachin Kumar <sinha1abc@gmail.com>
2026-01-28 03:14:59 +0530

    feat/matchmaking: implement interest and matchmaking strategies, with possible endpoints

---
+++ src/main/java/com/scriptbliss/bandhan/shared/exception/IllegalStateException.java
@@ -0,0 +1,7 @@
+package com.scriptbliss.bandhan.shared.exception;
+
+public class IllegalStateException extends RuntimeException {
+	public IllegalStateException(String message) {
+		super(message);
+	}
+}
